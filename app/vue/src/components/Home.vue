7f3f24e4f02756258bc96e705aef7dcbbd4a85e4
Ilona Zancaner <zancaneri@gmail.com>
2018-05-21 17:41:44 +0200

    Add basic css to frontend

--- app/vue/src/components/Home.vue
+++ app/vue/src/components/Home.vue
@@ -1,5 +1,21 @@
 <template>
   	<div id="home">
-		<h1>Home</h1>
+			<div class="container">
+				<h1>Ontdek evenementen bij jou in de buurt die matchen met jouw interesses</h1>
+				<form method="POST" action="" @submit.prevent="onSubmit">
+					<label for="location">Locatie</label>
+					<input type="text" id="location" name="location" placeholder="Waar zullen we zoeken?">
+					<label>Binnen 13 km</label>
+					<label>Datum</label>
+					<label>Interesses</label>
+					<button type="submit" class="btn primary-btn">Zoeken</button>
+			</form>
+			</div>
   </div>
 </template>
+
+<script></script>
+
+<style lang="scss">
+
+</style>
